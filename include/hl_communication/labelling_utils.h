de07e1ba22b320aed11609f9bf7d889a58356f0c
medrimonia <medrimonia@gmail.com>
2019-06-26 09:24:31 +0200

    Adding robot label to exportLabel

--- include/hl_communication/labelling_utils.h
+++ include/hl_communication/labelling_utils.h
@@ -10,6 +10,11 @@ namespace hl_communication
 bool sameBall(const BallMsg& msg1, const BallMsg& msg2);
 
 /**
+ * Return true if the two messages have the same id provided
+ */
+bool sameRobot(const RobotMessage& msg1, const RobotMessage& msg2);
+
+/**
  * Export the labelling stored in src to dst, overriding existing data if required
  * throws an error if src and dst have different frame indices
  */
--- src/hl_communication/labelling_utils.cpp
+++ src/hl_communication/labelling_utils.cpp
@@ -9,6 +9,15 @@ bool sameBall(const BallMsg& msg1, const BallMsg& msg2)
   return msg1.has_ball_id() && msg2.has_ball_id() && msg1.ball_id() == msg2.ball_id();
 }
 
+bool sameRobot(const RobotMessage& msg1, const RobotMessage& msg2)
+{
+  if (!msg1.has_robot_id() || !msg2.has_robot_id())
+    return false;
+  const RobotIdentifier& id1 = msg1.robot_id();
+  const RobotIdentifier& id2 = msg2.robot_id();
+  return id1.robot_id() == id2.robot_id() && id1.team_id() && id2.team_id();
+}
+
 void exportLabel(const LabelMsg& src, LabelMsg* dst)
 {
   // checking frame indices
@@ -45,6 +54,23 @@ void exportLabel(const LabelMsg& src, LabelMsg* dst)
     }
   }
   // TODO: Treat robots
+  for (const RobotMessage& new_robot : src.robots())
+  {
+    bool replaced = false;
+    for (int idx = 0; idx < dst->robots_size(); idx++)
+    {
+      RobotMessage* old_robot = dst->mutable_robots(idx);
+      if (sameRobot(new_robot, *old_robot))
+      {
+        replaced = true;
+        old_robot->CopyFrom(new_robot);
+      }
+    }
+    if (!replaced)
+    {
+      dst->add_robots()->CopyFrom(new_robot);
+    }
+  }
   // Export field matches
   for (const Match2D3DMsg& new_match : src.field_matches())
   {
