b51b35414359b0ba7b3b1b4f9e1179ab887e89e1
cgaspard3333 <cgaspard3333@gmail.com>
2026-06-08 10:33:43 +0200

    add Wrapper

--- examples/tp_dump_msg.cpp
+++ examples/tp_dump_msg.cpp
@@ -7,23 +7,26 @@
 #include <unistd.h>
 #include <iomanip>
 
-struct Stats{
+struct Stats
+{
   std::vector<hl_communication::GameMsg> msgs;
-  int totalBytes=0;
+  int totalBytes = 0;
 };
 
-struct Source{
+struct Source
+{
   uint64_t ip;
   uint32_t port;
-  bool operator<(const Source& other) const {
-    if (ip != other.ip) {
+  bool operator<(const Source& other) const
+  {
+    if (ip != other.ip)
+    {
       return ip < other.ip;
     }
     return port < other.port;
   }
-  rhoban_utils::TimeStamp firstMsg=rhoban_utils::TimeStamp::now();
+  rhoban_utils::TimeStamp firstMsg = rhoban_utils::TimeStamp::now();
 };
-
 
 int main(int argc, char** argv)
 {
@@ -36,28 +39,26 @@ int main(int argc, char** argv)
   std::cout << "Creating UDP Listener on port: " << port_read << std::endl;
   hl_communication::UDPMessageManager udp_receiver(port_read, -1);
 
+  rhoban_utils::TimeStamp printStats = rhoban_utils::TimeStamp::now();
 
-  rhoban_utils::TimeStamp printStats=rhoban_utils::TimeStamp::now();
-
-  std::cout << std::setprecision(2) << std::fixed ;
+  std::cout << std::setprecision(2) << std::fixed;
   hl_communication::GameMsg msg;
   while (true)
   {
-    if ((rhoban_utils::TimeStamp::now().getTimeSec() - printStats.getTimeSec()) > 5){
-        printStats = rhoban_utils::TimeStamp::now();
-        std::cout<<"* STATS *"<<std::endl;
-        std::cout << "Total players/sources: " << statsPerRobot.size() << std::endl;
-        for(auto& entry : statsPerRobot)
-        {
-          double duration = rhoban_utils::TimeStamp::now().getTimeSec() - entry.first.firstMsg.getTimeSec();
-          std::cout << "Player (" << entry.first.ip << ":" << entry.first.port << ") - " << entry.second.msgs.size() << 
-                       " messages, " << entry.second.totalBytes << 
-                       " bytes, msg/s: " << entry.second.msgs.size()/duration << 
-                       ", bytes/s: " << entry.second.totalBytes/duration << std::endl;
-        }
+    if ((rhoban_utils::TimeStamp::now().getTimeSec() - printStats.getTimeSec()) > 5)
+    {
+      printStats = rhoban_utils::TimeStamp::now();
+      std::cout << "* STATS *" << std::endl;
+      std::cout << "Total players/sources: " << statsPerRobot.size() << std::endl;
+      for (auto& entry : statsPerRobot)
+      {
+        double duration = rhoban_utils::TimeStamp::now().getTimeSec() - entry.first.firstMsg.getTimeSec();
+        std::cout << "Player (" << entry.first.ip << ":" << entry.first.port << ") - " << entry.second.msgs.size()
+                  << " messages, " << entry.second.totalBytes
+                  << " bytes, msg/s: " << entry.second.msgs.size() / duration
+                  << ", bytes/s: " << entry.second.totalBytes / duration << std::endl;
+      }
     }
-
-
 
     bool msg_received = udp_receiver.receiveMessage(&msg);
     if (!msg_received)
@@ -79,7 +80,8 @@ int main(int argc, char** argv)
       {
         std::cout << "\x1b[31mERROR: Received message without identifier\x1b[0m" << std::endl;
       }
-      if (!msg.has_robot_msg()){
+      if (!msg.has_robot_msg())
+      {
         std::cout << "\x1b[31mERROR: Message does not contain a robot message!\x1b[0m" << std::endl;
       }
       else
@@ -93,7 +95,8 @@ int main(int argc, char** argv)
           if (perception.has_ball_in_self())
           {
             const hl_communication::Position2d& ball = perception.ball_in_self();
-            std::cout << "\tBall position: (" << ball.x() << ", " << ball.y() << ") (quality: " << perception.ball_quality() << ")" << std::endl;
+            std::cout << "\tBall position: (" << ball.x() << ", " << ball.y()
+                      << ") (quality: " << perception.ball_quality() << ")" << std::endl;
           }
           else
           {
@@ -102,8 +105,8 @@ int main(int argc, char** argv)
           if (perception.has_self_in_field())
           {
             const hl_communication::Pose2d& self = perception.self_in_field();
-            std::cout << "\tSelf position: (" << self.position().x() << ", " << 
-                self.position().y() << "), dir: " << self.dir() <<" quality: "<<perception.field_quality()<< std::endl;
+            std::cout << "\tSelf position: (" << self.position().x() << ", " << self.position().y()
+                      << "), dir: " << self.dir() << " quality: " << perception.field_quality() << std::endl;
           }
           else
           {
@@ -115,7 +118,8 @@ int main(int argc, char** argv)
             for (int i = 0; i < perception.robots_size(); i++)
             {
               const hl_communication::Pose2d& robot = perception.robots(i);
-              std::cout << "\t - Robot " << i << ": (" << robot.position().x() << ", " << robot.position().y() << ")" << std::endl;
+              std::cout << "\t - Robot " << i << ": (" << robot.position().x() << ", " << robot.position().y() << ")"
+                        << std::endl;
             }
           }
           else
@@ -188,27 +192,31 @@ int main(int argc, char** argv)
           if (captain.has_ball())
           {
             const hl_communication::Position2d& ball = captain.ball().position();
-            std::cout << "\tCaptain ball position: (" << ball.x() << ", " << ball.y() << ") (votes: " << captain.ball().nb_votes() << ")" << std::endl;
+            std::cout << "\tCaptain ball position: (" << ball.x() << ", " << ball.y()
+                      << ") (votes: " << captain.ball().nb_votes() << ")" << std::endl;
           }
           else
           {
             std::cout << "\tNo ball in captain" << std::endl;
           }
-          std::cout << "\tCaptain opponents: "<<captain.opponents_size() << std::endl;
-          for(int i=0; i<captain.opponents_size(); i++){
+          std::cout << "\tCaptain opponents: " << captain.opponents_size() << std::endl;
+          for (int i = 0; i < captain.opponents_size(); i++)
+          {
             const hl_communication::Position2d& opponent = captain.opponents(i).pose().position();
-            std::cout << "\t - Opponent " << i << ": (" << opponent.x() << ", " << opponent.y() << ") (votes: " << captain.opponents(i).nb_votes() << ")" << std::endl;
+            std::cout << "\t - Opponent " << i << ": (" << opponent.x() << ", " << opponent.y()
+                      << ") (votes: " << captain.opponents(i).nb_votes() << ")" << std::endl;
           }
 
           std::cout << "\tCaptain orders: " << captain.orders_size() << std::endl;
           for (int i = 0; i < captain.orders_size(); i++)
           {
             const hl_communication::StrategyOrder& order = captain.orders(i);
-            std::cout << "\t - Order for robot " << order.robot_id() << ": action=" << hl_communication::Action_Name(order.action()) << std::endl;
+            std::cout << "\t - Order for robot " << order.robot_id()
+                      << ": action=" << hl_communication::Action_Name(order.action()) << std::endl;
             if (order.has_target_pose())
             {
-              std::cout << "\t\ttarget=(x=" << order.target_pose().position().x() << " y=" << order.target_pose().position().y()
-                        << " dir=" << order.target_pose().dir() << ")";
+              std::cout << "\t\ttarget=(x=" << order.target_pose().position().x()
+                        << " y=" << order.target_pose().position().y() << " dir=" << order.target_pose().dir() << ")";
             }
             std::cout << std::endl;
           }
@@ -217,6 +225,12 @@ int main(int argc, char** argv)
         {
           std::cout << "\tNo captain information in robot message" << std::endl;
         }
+        // print monitoring
+        auto& m = robot_msg.monitoring();
+        std::cout << "\tMonitoring: inGame=" << m.ingame() << " canMove=" << m.canmove() << " canKick=" << m.cankick()
+                  << " canScore=" << m.canscore() << std::endl;
+        std::cout << "\tstayOurField=" << m.stayourfield() << " ballClearing=" << m.ballclearing() << std::endl;
+        std::cout << "\trefereePhase=" << m.refereephase() << " captainPlacer=" << m.captainplacer() << std::endl;
       }
     }
   }
